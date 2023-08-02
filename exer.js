bf28abb918e5086a73c6737f88433516235c1746
Jakobson1983 <zytlinski.jakub@gmail.com>
2023-08-02 13:55:05 +0200

    Proba

---
+++ exer.js
@@ -0,0 +1,140 @@
+/*const pass = 'jakuhghgsffdfwww'
+
+if(pass.length > 10 && pass.includes('!')) {
+    console.log('dobre haslo');
+} else {console.log('masz za krotkie haslo');}*/
+
+/*function pressed(){
+    let text = document.getElementById('inp').value;
+    if (text === 'red'){
+        document.getElementById('header').style.color = 'red';
+    }else if(text==='green'){
+        document.getElementById('header').style.color = 'green';
+    }else if(text==='blue'){
+        document.getElementById('header').style.color = 'blue';
+    }else{
+        document.getElementById('header').style.color = 'black';
+    }
+}*/
+ /*const x = 100
+ if (x > 20 ) {
+    console.log('s(x)' > 20);
+ }else{
+    console.log('s(x)' < 20);
+ }
+
+ const newX = x > 20 ? 's{x} > 20' : 's{x} < 20'
+ console.log(newX);*/
+
+ /*const x = 50
+ const y = 30
+
+ if (x > y) {
+    console.log(`${x} jest wieksze niz ${y}`);
+ }*/
+
+/*let color = 'blue'
+ let newColor = 'green'
+
+ if (color === newColor) {
+    console.log('kolory sie zgadzaja');
+ }else{console.log('kolory sie nie zgadzaja');}*/
+
+ /*let x = 100
+ let y = 50
+
+ if (x > y) {
+    console.log('x > y');
+ }else if (x === y) {
+    console.log('x = y');
+ }else if (x < y) {
+    console.log('x < y');
+ }*/
+
+
+ /*let promo = '20%'
+ switch (promo) {
+    case '10%':
+        console.log('banany w promce po 10ct');
+        break;
+    case '30%':
+        console.log('mango dzisiaj w promce');
+    case '20%':
+        console.log('dzisiaj dobra promka');  
+    // case 'x': 
+    //console.log('mbjhvjhvgzj');  
+    default:
+        console.log('sorki, dzisiaj mamy to co mamy');
+        break;
+ }*/
+
+/*let x = 10
+
+ if (x % 2 === 0) {
+    console.log('liczby sa parzyste');
+ }*/
+
+ /*let x = 50
+
+ if (x >= 100) {
+    console.log(' x > 100 ');
+ }else if (x < 100 && x > 30) {
+    console.log(('x jest sredniakiem').toUpperCase());
+ }else if (x <= 30) {
+    console.log('x jest maly');
+ }*/
+
+ /*let x = 50;
+ let text;
+ if (x >= 100) {
+   text = ' x > 100 ';
+ }else if (x < 100 && x > 30) {
+    text = 'x jest sredniakiem';
+ }else if (x <= 30) {
+    text = 'x jest maly';
+ }
+
+ console.log(text.toUpperCase());*/
+
+//  const color = ['blue', 'white','red'];
+
+//  for (let i = 0; i < color.length; i++) {
+//    console.log(color[i]);
+   
+//  }
+
+
+// let miasta = ['Krakow', 'Warszawa', 'Opole', 'Lodz']
+
+// for (let i = 0; i < miasta.length; i++) {
+//    console.log(`to miasto nazywa sie ${miasta[i].toUpperCase()}`);
+   
+// }
+
+// let x = 0
+
+// while (x < 10) {
+//    x = x + 2
+//    console.log(x);
+// }
+
+// let x = 20
+// do { 
+//    x -= 3
+//    //console.log(x);
+// } while (x > 0);
+
+// console.log(x);
+
+// let numbers = [5,8,10,23,48,60]
+//  for (const number of numbers) {
+//    console.log(number / 5);
+//  }
+
+
+let numbers = [5,8,10,23,48,60]
+ for (const number of numbers) {
+   if (number %  2 === 0) {
+      
+   console.log(`liczba ${number} jest parzysta`);}
+ }
